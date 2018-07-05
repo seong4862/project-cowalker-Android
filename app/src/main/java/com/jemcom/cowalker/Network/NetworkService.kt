4457638ce9cd33c57ac77ad5c575320b3ed66375
Nuri Seong <nuli95@naver.com>
2018-07-06 04:24:10 +0900

    Revert "다시"
    This reverts commit 8f0c849508cc73b95f884909d86fefd91edc0b83.

--- app/src/main/java/com/jemcom/cowalker/Network/NetworkService.kt
+++ app/src/main/java/com/jemcom/cowalker/Network/NetworkService.kt
@@ -1,10 +1,8 @@
 package com.jemcom.cowalker.Network
 
 import com.jemcom.cowalker.Network.Get.Response.GetSignupResponse
-import com.jemcom.cowalker.Network.Post.PostJoin
 import com.jemcom.cowalker.Network.Post.PostLogin
 import com.jemcom.cowalker.Network.Post.PostSignup
-import com.jemcom.cowalker.Network.Post.Response.PostJoinResponse
 import com.jemcom.cowalker.Network.Post.Response.PostLoginResponse
 import com.jemcom.cowalker.Network.Post.Response.PostSignupResponse
 import retrofit2.Call
@@ -26,10 +24,4 @@ interface NetworkService {
     fun postLogin(
             @Body login : PostLogin
     ) : Call<PostLoginResponse>
-
-    @POST("/api/apply")
-    fun postJoin(
-            @Header("authorization") authorization : String,
-            @Body join : PostJoin
-    ) : Call<PostJoinResponse>
 }
